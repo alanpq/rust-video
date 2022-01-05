c9011b642730727014d41773ef4ddd60aeec9cdd
Alan Panayotov <alanpanayotov@gmail.com>
2022-01-05 05:07:08 +0000

    test ppm_get

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,19 @@ fn main() {
     let mut frame = FrameBuffer::new(50, 50);
     let mut out = stdout();
     let mut rng = rand::thread_rng();
+    for x in 0..frame.w {
+        for y in 0..frame.h {
+            frame.ppm_set(x, y, Color::from_rgb(&rng.gen(), &rng.gen(), &rng.gen()));
+        }
+    }
     loop {
         for x in 0..frame.w {
             for y in 0..frame.h {
-                frame.ppm_set(x, y, Color::from_rgb(&rng.gen(), &rng.gen(), &rng.gen()));
+                let (r,g,b) = frame.ppm_get(x, y).to_rgb();
+                let r = r * 0.9;
+                let g = g * 0.9;
+                let b = b * 0.9;
+                frame.ppm_set(x, y, Color::from_rgb(&r, &g, &b));
             }
         }
         frame.ppm_write(&mut out);
